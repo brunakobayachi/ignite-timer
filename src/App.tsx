901bb8846721328311a2a7bb5108eba25e3f2bcb
brunakobayachi <brunakobayachi@gmail.com>
2022-11-10 21:02:11 -0300

    feat header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  return (
-    <ThemeProvider theme = {defaultTheme}>
-        <Button variant="primary"/>
-        <Button variant="secondary"/>
-        <Button variant="success"/>
-        <Button variant="danger"/>
-        <Button/>
-    </ThemeProvider>
-    )
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
